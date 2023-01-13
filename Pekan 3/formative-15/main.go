497d61fbab4b4d9cd0d0f7357d75b67dd9f94c76
Kadigas <adbagas05@gmail.com>
2023-01-14 03:14:04 +0700

    feat: added formative-15

--- Pekan 3/formative-15/main.go
+++ Pekan 3/formative-15/main.go
@@ -3,16 +3,14 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"formative-15/controllers"
+	"formative-15/database"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "password"
-	dbname   = "practice"
 )
 
 var (
@@ -21,17 +19,37 @@ var (
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+
+	//ENV Configuration
+	err = godotenv.Load("config/.env")
+	if err != nil {
+		fmt.Println("failed load file environment")
+	} else {
+		fmt.Println("success read file environment")
+	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 
 	DB, err = sql.Open("postgres", psqlInfo)
 	err = DB.Ping()
 	if err != nil {
-		fmt.Println("DB conenction failed")
+		fmt.Println("DB connection failed")
 		panic(err)
 	} else {
 		fmt.Println("DB Connection Success")
 	}
 
+	database.DbMigrate(DB)
+
 	defer DB.Close()
+
+	//Router
+	router := gin.Default()
+	router.GET("/persons", controllers.GetAllPerson)
+	router.POST("/persons", controllers.InsertPerson)
+	router.PUT("/persons/:id", controllers.UpdatePerson)
+	router.DELETE("/persons/:id", controllers.DeletePerson)
+
+	router.Run("localhost:8080")
 }
