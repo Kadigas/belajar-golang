b67b2f5c1844c31fce53f0a0d2e885c53aaafec1
Kadigas <adbagas05@gmail.com>
2023-01-13 23:55:31 +0700

    feat: retype password

---
+++ Pekan 3/formative-15/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "password"
+	dbname   = "practice"
+)
+
+var (
+	DB  *sql.DB
+	err error
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	DB, err = sql.Open("postgres", psqlInfo)
+	err = DB.Ping()
+	if err != nil {
+		fmt.Println("DB conenction failed")
+		panic(err)
+	} else {
+		fmt.Println("DB Connection Success")
+	}
+
+	defer DB.Close()
+}
